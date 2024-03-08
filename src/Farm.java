a16c721ba210894c56793b6688256f3fa783dccc
Fudr007 <valenta2@spsejecna.cz>
2024-03-08 13:40:33 +0100

    added class Currant added constructor to Flower added setters and getters to Flower added method waters to FLowe added class Potato added clas Tomato added cals Cucumber added constructor to Animal added setters and getters to Animal added class Cow added class Sheep added class Chicken added method addAnimal added method fullBarn

--- src/Farm.java
+++ src/Farm.java
@@ -1,5 +1,45 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Farm {
 
+    private int money;
+    private ArrayList<Animal> animals = new ArrayList<>();
+    private ArrayList<Flower> flowers = new ArrayList<>();
+
+    public void addAnimal(Animal animal){
+        if (){
+
+        }
+    }
+
+    public String fullBarn(){
+        String reply = "";
+        int countB = 0;
+        int countS = 0;
+        if (animals.isEmpty()){
+            return "no";
+        }else {
+            for (int i = 0; i<animals.size();i++){
+                if (animals.get(i).getSize().equals(Size.BIG)){
+                    countB++;
+                }
+                if (animals.get(i).getSize().equals(Size.SMALL)){
+                    countS++;
+                }
+            }
+        }
+        if (countS == 10){
+            reply = "small";
+        }
+        if (countB == 10){
+            reply = "big";
+        }
+        if (countS == 10 && countB == 10){
+            reply = "both";
+        }
+        return reply;
+    }
 
 
 }
