84490a5ef5c01acfc66e5b98f2d663e365abb259
Fudr007 <valenta2@spsejecna.cz>
2024-03-08 13:06:29 +0100

    added class Currant added constructor added method waters

--- src/Currant.java
+++ src/Currant.java
@@ -1,2 +1,14 @@
-public class Currant {
+
+public class Currant extends Flower{
+
+    public Currant() {
+        super("Currant", 20, 2, 40);
+    }
+
+    @Override
+    public void waters(){
+        this.chanceOfGrowth += 5;
+    }
+
 }
+
